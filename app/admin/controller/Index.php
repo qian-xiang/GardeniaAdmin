7d85e4b0926c26fbe4a19689aebd68b95f692c51
qian-xiang <847882429@qq.com>
2020-07-15 09:01:15 +0800

    正在完成GardeniaForm表单构造器

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -8,26 +8,21 @@
  */
 namespace app\admin\controller;
 
-use app\admin\extend\core\core_class\GardeniaTable;
-use app\admin\extend\diy\extra_class\AppConstant;
+
+//use app\admin\extend\core\core_class\GardeniaTable;
+//use app\admin\extend\diy\extra_class\AppConstant;
 use app\BaseController;
+use \gardenia_admin\src\core\core_class\GardeniaForm;
 use \think\facade\Db;
 
 class Index extends BaseController
 {
     public function index()
     {
-        $list = Db::name(AppConstant::TABLE_USER)->select()->toArray();
-        $gardeniaTable = new GardeniaTable();
-        return $gardeniaTable->addTableHeader('id','ID')
-            ->addTableHeader('username','用户名')
-            ->addTableHeader('password','密码')
-            ->addTableHeader('login_status','登录状态')
-            ->addTableHeader('login_code','登录token')
-//            ->addTableHeader('create_time','创建时间')
-            ->setData($list)->display();
+//        $list = Db::name(AppConstant::TABLE_USER)->select()->toArray();
+        $gardeniaTable = new GardeniaForm();
+        return $gardeniaTable->display();
 
-//        return view('index');
     }
 
     public function hello($name = 'ThinkPHP6')
