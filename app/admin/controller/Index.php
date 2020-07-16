0df191039ab9590d83b3d7311d974eb2c7d5ebe4
qian-xiang <847882429@qq.com>
2020-07-17 00:25:11 +0800

    在表单和列表构造器中直接渲染视图

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -11,19 +11,18 @@ namespace app\admin\controller;
 
 //use app\admin\extend\core\core_class\GardeniaTable;
 //use app\admin\extend\diy\extra_class\AppConstant;
-use app\BaseController;
 use \gardenia_admin\src\core\core_class\GardeniaForm;
 use \think\facade\Db;
 
-class Index extends BaseController
+class Index
 {
     public function index()
     {
 //        $list = Db::name(AppConstant::TABLE_USER)->select()->toArray();
         $gardeniaForm = new GardeniaForm();
         $gardeniaForm = $gardeniaForm->addFormItem('用户名啊啊','text','username','嘿嘿和')
-            ->addFormItem('是否是发','switch','switch1');
-        return $gardeniaForm->display();
+            ->addFormItem('ooo','switch','switch1');
+        $gardeniaForm->display();
 
     }
 
